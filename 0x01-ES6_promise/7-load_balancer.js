52560a6eca3b0fc9b8db5a25204f924f62fb5180
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-04-12 11:59:57 +0000

    Es6_promises

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,8 @@
+
+// Write and export a function named loadBalancer
+// It should accept two arguments chinaDownload (Promise)
+// and USDownload (Promise).
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
